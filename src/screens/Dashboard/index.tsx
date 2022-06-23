3c7770bea9e1f27deef3db26e1b4efc70680161c
Tiago <tiago.gpes@gmail.com>
2022-06-23 20:31:20 -0300

    Cap. 2 - Style Componentes - Utilizado proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,37 @@
+/**
+ * Biliotecas utilizadas:
+ * - yarn add react-native-responsive-fontsize
+*/
 import React from 'react';
 
+import { Text, View } from 'react-native';
+
 import {
-    Container, Title
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    
 } from './styles';
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/46384388?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Tiago</UserName>
+                        </User>
+                    </UserInfo> 
+                </UserWrapper>         
+            </Header>
         </Container>
     )
 }
