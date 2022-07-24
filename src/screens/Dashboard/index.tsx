dd2cc4e939c9b2eab4a6b1ab11c9a8486e7e6e65
Tiago <tiago.gpes@gmail.com>
2022-07-24 11:20:08 -0300

    Cap. 2 - Styled Componentes - Tipando e acessando propriedades no Styled Components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -14,7 +14,8 @@ import {
     User,
     UserGreeting,
     UserName,
-    Icon
+    Icon,
+    HighlightCards
 } from './styles';
 
 export function Dashboard(){
@@ -32,8 +33,26 @@ export function Dashboard(){
                     <Icon name="power"/>
                 </UserWrapper>         
             </Header>
-
-            <HighlightCard />
+            <HighlightCards>
+                <HighlightCard 
+                    type="up"
+                    title="Entradas" 
+                    amount="R$ 17.400,00" 
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard 
+                    type="down"
+                    title="Saídas" 
+                    amount="R$ 1.259,00" 
+                    lastTransaction="Última entrada dia 03 de abril"
+                />
+                <HighlightCard 
+                    type="total"
+                    title="Total" 
+                    amount="R$ 16.141,00" 
+                    lastTransaction="01 à 16 de abril"
+                />
+            </HighlightCards>
         </Container>
     )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
     flex: 1;
@@ -15,7 +16,7 @@ export const Header = styled.View`
     background-color: ${({ theme }) => theme.colors.primary};
 
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
     flex-direction:  row;
 `;
 
@@ -23,6 +24,7 @@ export const UserWrapper = styled.View`
     width: 100%;
     //cima- baixo- direta - esquerda
     padding: 0 24px;
+    margin-top: ${getStatusBarHeight()+RFValue(28)}px;
 
     flex-direction: row;
     justify-content: space-between;
@@ -63,4 +65,14 @@ export const UserName = styled.Text`
 export const Icon = styled(Feather)`
     color: ${({ theme })=> theme.colors.secondary};
     font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards= styled.ScrollView.attrs({
+    horizontal:true,
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle:{paddingHorizontal: 24}
+})`
+    width: 100%;
+    position: absolute;
+    margin-top:${RFPercentage(20)}px;
 `;
